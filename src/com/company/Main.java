f43038f73d761aab40c5b359447931a73d6baf36
Yevhenii Gruca <ygruca@deloitte.ua>
2022-09-30 12:28:25 +0300

    replace with compare

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        List<Integer> left = new ArrayList<>(List.of(3, 5));
+        List<Integer> left = new ArrayList<>(List.of(1, 7, 23));
         List<Integer> right = new ArrayList<>(List.of(2, 4, 7, 10, 11));
         List<Integer> list3 = new ArrayList<>();
 
@@ -19,7 +19,7 @@ public class Main {
                 list3.addAll(leftPointer == left.size() ? right.subList(rightPointer, right.size()) : left.subList(leftPointer, left.size()));
                 break;
             }
-            if (left.get(leftPointer) <= right.get(rightPointer)) {
+            if (left.get(leftPointer).compareTo(right.get(rightPointer)) <= 0) {
                 list3.add(left.get(leftPointer++));
             } else {
                 list3.add(right.get(rightPointer++));
